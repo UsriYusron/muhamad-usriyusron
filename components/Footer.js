15699a4b52840f0c037b4c80259855c2138a3fea
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2026-03-01 21:10:31 +0700

    refactor: remove unused PixelTrail component and clean up Footer content

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ export function Footer() {
             <div className="h-[150dvh] w-full">
                 <div className="bg-neutral-200 dark:bg-neutral-800 rounded-3xl h-full flex flex-col items-center justify-center">
                     <p className="text-xl uppercase text-neutral-950 dark:text-neutral-200 font-medium">
-                        some content
+                        {/* some content */}
                     </p>
                 </div>
             </div>
